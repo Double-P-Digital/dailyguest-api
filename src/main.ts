113c18d300804acc53d8eb67c36c2c5f9ba46dde
parnau-patrick <parnau.patrick@yahoo.com>
2025-12-10 10:30:19 +0200

    enable cors for domain

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3001'],
+    origin: ['http://localhost:5173', 'http://localhost:3001','https://dailyguest.online',
+    'https://www.dailyguest.online',],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
     credentials: true,
